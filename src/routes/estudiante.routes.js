37ab1b2297802b9c4f975d039e2e6b41b6043d3e
ReynerBadillaArias <reyner.badilla.arias@est.una.ac.cr>
2022-08-20 21:08:08 -0600

    Nueva base

--- src/routes/estudiante.routes.js
+++ src/routes/estudiante.routes.js
@@ -1,12 +1,8 @@
 import { Router } from "express";
-import { methods as estudianteController } from "./../controller/estudiante.controller";
+import { getEstudianteId } from "./../controller/estudiante.controller";
 
 const router = Router();
 
-router.get("/", estudianteController.getEstudiantes);
-router.get("/:id", estudianteController.getEstudianteId);
-router.post("/", estudianteController.postEstudiante);
-router.put("/", estudianteController.updateEstudiante);
-router.delete("/:id", estudianteController.deleteEstudiante);
+router.get("/:id", getEstudianteId);
 
 export default router;
--- src/routes/grado.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { methods as gradoController } from "./../controller/grado.controller";
-
-const router = Router();
-
-router.post("/", gradoController.postGrado);
-
-export default router;
